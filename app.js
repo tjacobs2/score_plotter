72ef4161e6a4a4bc2df681b688ee8d7572ae8d81
Tim Jacobs <timjacobs2@gmail.com>
2014-04-07 17:39:39 -0400

    So, simplify things a bit, but then make them more complicated with angular

--- app.js
+++ app.js
@@ -2,46 +2,16 @@
  * Module dependencies
  */
 var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
   , logger = require('morgan')
   , fs = require('fs');
 
-var app = express()
+var app = express();
+app.use(express.static(__dirname + '/public')) //set static file location as the public directory
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(logger('dev')) //Log every request
 
-//Custom compile to add nib
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
-
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-
-//Use dev logging (log every GET/POST)
-app.use(logger('dev'))
-
-//Add stylus, convering .styl to CSS. Add
-//a custom compile fucntion so we can use
-//the nib add-on
-app.use(stylus.middleware({
-  src: __dirname + '/views',
-  dest: __dirname + '/public'
-  , compile: compile
-  }
-));
-
-//Tell Express that we're serving up static files
-app.use(express.static(__dirname + '/public'))
-
-// dynamically include routes (Controller)
-fs.readdirSync('./controllers').forEach(function (file) {
-  if(file.substr(-3) == '.js') {
-      route = require('./controllers/' + file);
-      route.controller(app);
-  }
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html');
 });
 
 app.listen(3000)
